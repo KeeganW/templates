812e3ba58552d663d0920c79c2ace3c009856a88
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-07-21 16:54:29 -1000

    ci(flask): documentation changes to pass lint checker

--- flask/application/hello/hello.py
+++ flask/application/hello/hello.py
@@ -1,3 +1,6 @@
+"""
+A simple example of a blueprint for saying hello to a new website visitor.
+"""
 from flask import Blueprint, render_template, current_app
 
 
@@ -6,6 +9,12 @@ hello_blueprint = Blueprint('hello', __name__, template_folder='templates')
 
 @hello_blueprint.route('/')
 @hello_blueprint.route('/<name>')
-def hello(name=None):
+def hello(name: str = None):
+    """
+    Say hello to a new user!
+
+    :param name: optional, the name of the new user
+    :return: A rendering of the hello page, with provided data.
+    """
     current_app.logger.info("Name provided was: {}".format(name))
     return render_template('hello.html', name=name)
