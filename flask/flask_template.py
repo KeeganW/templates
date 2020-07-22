ea5e36d65bb8f1b35a3c305b363674beb8443957
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-07-21 17:20:49 -1000

    fix(flask): fixing lint errors
    - added initial docstring
    - renamed variables for hosting services which prefer `application` over `app` for the main
    - added shebang line

--- flask/flask_template.py
+++ flask/flask_template.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+"""
+The main starting file for this flask project, which is in charge of creating the application and
+starting it based off of configuration and environment variables.
+"""
 import os
 
 from logging.config import dictConfig
 from flask import Flask, render_template
+from flask.logging import create_logger
+
 from config import config
 from application.hello.hello import hello_blueprint
 
@@ -24,37 +31,38 @@ def create_application():
             'formatter': 'default'
         }},
         'root': {
-            'level': 'DEBUG' if os.getenv('DEBUG', False) else 'INFO',
+            'level': 'DEBUG' if bool(os.getenv('DEBUG', 'False')) else 'INFO',
             'handlers': ['wsgi']
         }
     })
 
     # Initialize the app
-    application = Flask(__name__)
+    app = Flask(__name__)
+    log = create_logger(app)
 
     # Secret Key
-    application.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(32))
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(32))
 
     # Debug
     env = os.getenv('ENVIRONMENT', 'production')
-    application.config['ENV'] = env
-    application.config['DEBUG'] = os.getenv('DEBUG', config[env]['debug'])
+    app.config['ENV'] = env
+    app.config['DEBUG'] = os.getenv('DEBUG', config[env]['debug'])
 
     # Link to all the other components
-    application.register_blueprint(hello_blueprint, url_prefix='/hello')
+    app.register_blueprint(hello_blueprint, url_prefix='/hello')
 
     # Main route
-    @application.route("/")
+    @app.route("/")
     def index():
-        application.logger.debug('Running the main page. Application is running as a {} server.'.format(env))
+        log.debug('Running the main page. Application is running as a {} server.'.format(env))
         return render_template('layout.html')
 
-    return application
+    return app, log
 
 
 if __name__ == "__main__":
     # Get and start the flask application
-    app = create_application()
+    application, logger = create_application()
     environment = os.getenv('ENVIRONMENT', 'production')
 
     # Configure host and port
@@ -64,7 +72,7 @@ if __name__ == "__main__":
     # Depending on the environment, run as a waitress server or a flask server
     if environment == 'production':
         from waitress import serve
-        serve(app, host=host, port=port)
-        app.logger.debug('Application is running at http://{}:{}'.format(host, port))
+        serve(application, host=host, port=port)
+        logger.debug('Application is running at http://{}:{}'.format(host, port))
     else:
-        app.run(host, port)
+        application.run(host, port)
